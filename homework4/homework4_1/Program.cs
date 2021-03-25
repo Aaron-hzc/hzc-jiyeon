017042adb346cb76246dd87922e2cdab47e35499
Aaron-hzc <79973116+Aaron-hzc@users.noreply.github.com>
2021-03-26 00:24:09 +0800

    Add files via upload

---
+++ homework4/homework4_1/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homework4_1
+{
+    class Node<T>
+    {
+        public Node<T> Next { get; set; }
+        public T Data { get; set; }
+        
+        public Node(T t)
+        {
+            Next = null;
+            Data = t;
+        }
+        public static void Each(GenericList<T> nodes, Action<T> f)
+        {
+            Node<T> node = nodes.Head;
+            while (node != null)
+            {
+                f(node.Data);
+                node = node.Next;
+            }
+        }
+    }
+
+    class GenericList<T>
+    {
+        private Node<T> head;
+        private Node<T> tail;
+        public GenericList()
+        {
+            tail = head = null;
+        }
+        public Node<T> Head
+        {
+            get => head;
+        }
+        public void Add(T t)
+        {
+            Node<T> n = new Node<T>(t);
+            if (tail == null)
+                head = tail = n;
+            else
+            {
+                tail.Next = n;
+                tail = n;
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GenericList<int> nodes = new GenericList<int>();
+            Random r = new Random();
+            for(int i = 0; i < 10; i++)
+            {
+                nodes.Add(r.Next(0, 100));
+            }
+
+            int sum = 0, max = -1, min = 101;
+            Console.WriteLine("链表元素为：");
+                Node<int>.Each(nodes, m => Console.Write(m + " "));
+            Console.WriteLine();
+            Node<int>.Each(nodes, m => sum += m);
+            Console.WriteLine($"和为：{sum}");
+            Node<int>.Each(nodes, m => min = Math.Min(min, m));
+            Console.WriteLine($"最小值为：{min}");
+            Node<int>.Each(nodes, m => max = Math.Max(max, m));
+            Console.WriteLine($"最大值为：{max}");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ homework4/homework4_2/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homework4_2
+{
+    public class AlarmEventArgs
+    {
+        public static string Time { get; set; }
+    }
+
+    public delegate void AlarmEventHandler(object sender, AlarmEventArgs e);
+
+    public class AlarmClock
+    {
+        public event AlarmEventHandler onAlarm;
+        public void alarm(String time)
+        {
+            AlarmEventArgs args = new AlarmEventArgs();
+            AlarmEventArgs.Time = time;
+            while (true) onAlarm(this, args);
+        }
+    }
+    public class Form
+    {
+        public AlarmClock clock = new AlarmClock();
+        public Form()
+        {
+            clock.onAlarm += Tick;
+            clock.onAlarm += Alarm;
+        }
+
+        public void Tick(object sender, AlarmEventArgs args)
+        {
+            System.Threading.Thread.Sleep(1000);
+            Console.WriteLine("滴答");
+        }
+        public void Alarm(object sender, AlarmEventArgs args)
+        {
+            if (AlarmEventArgs.Time == DateTime.Now.ToLongTimeString().ToString())
+            {
+                System.Threading.Thread.Sleep(1000);
+                Console.WriteLine($"{AlarmEventArgs.Time}!时间到了!");
+            }
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string time = "10:43:00";
+            Form form = new Form();
+            form.clock.alarm(time);
+        }
+    }
+}
