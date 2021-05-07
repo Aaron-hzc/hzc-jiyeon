2088f46a0f6fe5275ea5f44bc9da611398549955
Aaron-hzc <79973116+Aaron-hzc@users.noreply.github.com>
2021-05-07 16:22:05 +0800

    修改版

--- homework9/homework9/Form1.cs
+++ homework9/homework9/Form1.cs
@@ -13,13 +13,13 @@ namespace homework9
 {
     public partial class Form1 : Form
     {
-        public newCrawler myCrawler;
+        public Crawler myCrawler;
         public Thread thread;
         bool crawl;
         public Form1()
         {
             InitializeComponent();
-            myCrawler = new newCrawler();
+            myCrawler = new Crawler();
             crawl = false;
         }
 
@@ -31,7 +31,7 @@ namespace homework9
         private void button1_Click(object sender, EventArgs e)
         {
             crawl = true;
-            thread = new Thread(myCrawler.Crawl);
+            thread = new Thread(myCrawler.Start);
             thread.Start(textBox1.Text);
         }
 
@@ -58,7 +58,7 @@ namespace homework9
 
         private void timer1_Tick(object sender, EventArgs e)
         {
-            richTextBox1.Text = myCrawler.Infor;
+            richTextBox1.Text = myCrawler.ToString(DownloadedPages<string,true>);
         }
     }
 }
